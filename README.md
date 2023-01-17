3bdfe0df2122fb08609fd9478018620dd32aef36
textcunma <78415309+textcunma@users.noreply.github.com>
2023-01-17 11:13:30 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # convertPDF-ENJP
 PDFファイルの文章を英語から日本語に変換
+
+## 概要
+PDFMinerライブラリとEasyNMTライブラリを組み合わせて作成した簡易的なコード。
+PDFファイルを入力, 出力は日本語文。
+文書によっては処理が比較的長いですのでご注意を。
+
+## EasyNMTライブラリ
+英語から複数の言語に翻訳可能なライブラリ。OpenAIのAPIを用いて翻訳も可能ですが有料ですので、お試しに作る際にはこのライブラリが有効かと思います。
+
+## 仮想環境構築
+
+1. venvを用いて構築<br>
+「convertpdf」という仮想環境を作成。
+     ``` bash
+     python -m venv convertpdf
+     ```
+
+2. 環境を有効化
+
+     ``` bash
+     // Windows
+     .\convertpdf\Scripts\activate
+
+     // Linux
+     source convertpdf/bin/activate
+     ```
+
+3. ライブラリをインストール
+     ``` bash
+     pip install -U easynmt
+     pip install -U protobuf~=3.20.0
+     pip install pdfminer.six
+     ```
+     その他、「shutil」等の些細なライブラリをインストールする必要がありますが説明は省きます
+
+4. 実行
+     ``` bash
+     // サンプル
+     python main.py --fileurl https://arxiv.org/pdf/2006.11693v2.pdf
+     ```
+
+## 参考サイト
+- https://tech.excite.co.jp/entry/2022/08/19/191151
+- https://www.shibutan-bloomers.com/python_library_pdfminer-six/2124/
+- https://docs.python.org/ja/3/howto/urllib2.html
+
+
---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+import tempfile
+import argparse
+import urllib.request
+
+from io import StringIO
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+
+from easynmt import EasyNMT
+from tqdm import tqdm
+
+def main(args):
+
+	with urllib.request.urlopen(args.fileurl) as response:
+		print("file name: ", args.fileurl)
+		with tempfile.NamedTemporaryFile(delete = False) as tmp_file:
+			shutil.copyfileobj(response, tmp_file)
+
+		with open(tmp_file.name, 'rb') as fp:
+			print("file open")
+			outfp = StringIO()
+			rmgr = PDFResourceManager()
+			lprms = LAParams()
+			device = TextConverter(rmgr, outfp, laparams = lprms) 
+			iprtr = PDFPageInterpreter(rmgr, device)
+
+			# 1ページずつ解析
+			for page in PDFPage.get_pages(fp):
+				iprtr.process_page(page)
+
+			text = outfp.getvalue()
+
+			outfp.close()
+			device.close()
+
+		# 改行コードで分割
+		lines = text.splitlines()
+
+		# モデル読み込み
+		model = EasyNMT('mbart50_m2m')
+
+		# 英語から日本語へ変換
+		jplines = ''
+		for line in tqdm(lines):
+			if len(line) <= 1:
+				continue
+			line = model.translate(line, target_lang = 'ja')
+			jplines += line + '\n'
+
+		print(jplines)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description = 'PDFファイルを英語から日本語に変換')
+	parser.add_argument('--fileurl', default = 'https://arxiv.org/pdf/2006.11693v2.pdf', help = 'PDFファイルURL')
+	args = parser.parse_args()
+
+	try:
+		if not os.path.splitext(args.fileurl)[-1] == '.pdf':
+			raise Exception('Error!')
+	except Exception:
+		print("This file is not PDF")
+		exit(1)
+
+	main(args)
